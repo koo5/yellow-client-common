efcc7c91dbcdf697203b5638045929e4330a9af1
Jindrich Kolman <kolman.jindrich@gmail.com>
2025-05-24 12:11:38 +0200

    test

---
+++ src/components/button/Button.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+
+	let { onClick, label } = $props();
+
+</script>
+
+bbb<button
+	class="button"
+	on:click={onClick}
+>
+	{#if label}
+		<span class="label">{label}</span>
+	{/if}
+</button>button
